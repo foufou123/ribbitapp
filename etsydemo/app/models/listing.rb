1a5f33166e31f3a20a0c7ae9f1cb6b432bd66456
Raouf Nabi <raouf.nabi@gmail.com>
2014-09-06 16:20:31 -0400

    updated listing.rb

--- etsydemo/app/models/listing.rb
+++ etsydemo/app/models/listing.rb
@@ -1,3 +1,3 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :style => { :medium => '200x', :thumb => "100x100>" }, :default_url => "default.jpg"
+  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
 end
